aa748a06c7876e8ea573ccdce56b09951bc64035
Faith Omotoso <faithomotoso@gmail.com>
2020-02-29 11:11:04 +0100

    Initial Commit

---
+++ lib/helpers/helpers.dart
@@ -0,0 +1,16 @@
+// this file contains mostly variables to be reused and stored here for easy management
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+// primary colors
+final Color primaryColor = Color(0xff00acc1);
+final Color primaryColorLight = Color(0xff5ddef4);
+final Color primaryColorDark = Color(0xff007c91);
+
+// secondary colors
+final Color secondaryColor = Color(0xffe91e63);
+final Color secondaryColorLight = Color(0xffff6090);
+final Color secondaryColorDark = Color(0xffb0003a);
+
